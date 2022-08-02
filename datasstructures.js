98bd65a45a6ab8f3e5a9c22a09c122b852b30b39
Janice-M <janiceink@gmail.com>
2022-08-02 06:42:01 +0300

    positive sum

--- datasstructures.js
+++ datasstructures.js
@@ -98,9 +98,10 @@ function even_or_odd(number) {
   }
 
 
-  //
+  // You get an array of numbers, return the sum of all of the positives ones.
 
-  ///
+  // Example [1,-4,7,12] => 1 + 7 + 12 = 20
+  // Note: if there is nothing to sum, the sum is default to 0.
 
   function positiveSum(arr) {
     let sum = 0;
