32f4074b4fb0f8f755789344c0661267726836ce
Janice-M <janiceink@gmail.com>
2022-08-02 06:38:52 +0300

    positive sum

--- datasstructures.js
+++ datasstructures.js
@@ -96,3 +96,18 @@ function even_or_odd(number) {
     
     
   }
+
+
+  //
+
+  ///
+
+  function positiveSum(arr) {
+    let sum = 0;
+  
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) sum += arr[i]
+    }
+    return sum
+
+}
