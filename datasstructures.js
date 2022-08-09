3a6efa7558b9d775aa007487e6350da837875f34
Janice-M <janiceink@gmail.com>
2022-08-09 15:31:31 +0300

    multiples of three and five

--- datasstructures.js
+++ datasstructures.js
@@ -137,3 +137,13 @@ function noSpace(x) {
     }
     return newStr
 }
+
+// find multiples of three and Five
+function solution(number){
+  let sum = 0;    for(let i=1; i<number; i++) {        
+    if(i % 3 === 0 || i % 5 === 0){            
+      sum += i;        
+    }    
+  }    
+  return sum;
+}
