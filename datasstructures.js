019defd7c72a3be642b974f2caadb6ed4cc1cb05
Janice-M <janiceink@gmail.com>
2022-08-12 12:23:00 +0300

    PIGLATIN 1

--- datasstructures.js
+++ datasstructures.js
@@ -147,3 +147,7 @@ function solution(number){
   }    
   return sum;
 }
+
+// PIGLATIN QUIZ
+//Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
