1c0653527e7e14f555f0aa8bd6d71d447113decb
Janice-M <janiceink@gmail.com>
2022-08-17 16:57:12 +0300

    phonenumber

--- datasstructures.js
+++ datasstructures.js
@@ -148,6 +148,8 @@ function solution(number){
   return sum;
 }
 
-// PIGLATIN QUIZ
-//Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 
+//Example:
+
+//createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
