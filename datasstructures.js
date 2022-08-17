b89fbe42fc0a343548a79e06fbfc463075c778c4
Janice-M <janiceink@gmail.com>
2022-08-17 17:01:04 +0300

    phonenumber

--- datasstructures.js
+++ datasstructures.js
@@ -153,3 +153,14 @@ function solution(number){
 //Example:
 
 //createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+function createPhoneNumber(numbers){
+  let format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
