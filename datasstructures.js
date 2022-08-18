cd205ab14ea71840eab175d17591c12ccc084495
Janice-M <janiceink@gmail.com>
2022-08-18 16:34:50 +0300

    pangram

--- datasstructures.js
+++ datasstructures.js
@@ -168,3 +168,15 @@ function createPhoneNumber(numbers){
 // A pangram is a sentence that contains every single letter of the alphabet at least once.
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
+function isPangram(string){
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const str = string.toLowerCase();
+  
+  for (let i = 0; i < alphabet.length-1; i += 1) {
+    if (str.indexOf(alphabet[i]) === -1) {
+      return false;
+    }
+  }
+  
+  return true;
+}
