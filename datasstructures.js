1ee6befa79f61fe559db51e6551bf648ad31b9ca
Janice-M <janiceink@gmail.com>
2022-08-18 16:19:01 +0300

    pangram

--- datasstructures.js
+++ datasstructures.js
@@ -164,3 +164,7 @@ function createPhoneNumber(numbers){
   
   return format;
 }
+
+// A pangram is a sentence that contains every single letter of the alphabet at least once.
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
