77297f5df6f16db65001487db4c07ce533c26f25
Janice-M <janiceink001@gmail.com>
2022-08-24 15:46:09 +0300

    range extraction

--- datasstructures.js
+++ datasstructures.js
@@ -302,3 +302,27 @@ function solution(list){
  }
  return list.join();
 }
+
+function solution(individualIntegers) {
+  return individualIntegers
+    .reduce(splitIntoRanges, [])
+    .map(convertToRange)
+    .join(",");
+}
+
+function splitIntoRanges(ranges, number) {
+  if (!ranges.length) {
+    ranges.push([number]);
+    return ranges;
+  }
+
+  var lastRange = ranges[ranges.length - 1];
+  var lastNumber = lastRange[lastRange.length - 1];
+
+  number === lastNumber + 1 ? lastRange.push(number) : ranges.push([number]);
+  return ranges;
+}
+
+function convertToRange(range) {
+  return range.length < 3 ? range.join(",") : range[0] + "-" + range[range.length - 1];
+}
