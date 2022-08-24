823b40f663a23283214b93bcb3c775db90939577
Janice-M <janiceink001@gmail.com>
2022-08-24 15:45:00 +0300

    range extraction

--- datasstructures.js
+++ datasstructures.js
@@ -262,3 +262,33 @@ function smallEnough(a, limit){
   return a.every(x => x <= limit);
 }
 
+
+// Range Extraction
+
+function rangeExtraction(list) {
+  var len = list.length;
+  var out = [];
+  var i, j;
+ 
+  for (i = 0; i < len; i = j + 1) {
+    // beginning of range or single
+    out.push(list[i]);
+ 
+    // find end of range
+    for (var j = i + 1; j < len && list[j] == list[j-1] + 1; j++);
+    j--;
+ 
+    if (i == j) {
+      // single number
+      out.push(",");
+    } else if (i + 1 == j) {
+      // two numbers
+      out.push(",", list[j], ",");
+    } else { 
+      // range
+      out.push("-", list[j], ",");
+    }
+  }
+  out.pop(); // remove trailing comma
+  return out.join("");
+}
