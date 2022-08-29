5d06523f72b1c5b4043d65cc6435dba410beec85
Janice-M <janiceink001@gmail.com>
2022-08-29 15:37:48 +0300

    scramblies

--- datasstructures.js
+++ datasstructures.js
@@ -433,4 +433,10 @@ function scramble(str1, str2) {
    return true;
  }
 
- 
+ // good practise i
+
+ function scramble(str1, str2) {
+  let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
+  return str2.split("").every((character) => --occurences[character] >= 0);
+}
+
