04121bc2121088fc4ad620240075f8a5fad690dd
Janice-M <janiceink001@gmail.com>
2022-08-29 15:37:15 +0300

    scramblies

--- datasstructures.js
+++ datasstructures.js
@@ -407,4 +407,30 @@ Complete the function scramble(str1, str2) that returns true if a portion of str
 can be rearranged to match str2, otherwise returns false.
 */
 
-//less efficient:
+//mine - less efficient:
+
+function scramble(str1, str2) {
+  
+  const reducer = (container, element) => {
+     if(container[element] === undefined) {
+       container[element] = 1;
+       return container;
+     } else {
+       container[element] += 1;
+       return container;
+     }
+   }
+   
+   var container1 = str1.split('').reduce(reducer,{});
+   
+   var container2 = str2.split('').reduce(reducer,{});
+   
+   for (var prop in container2) {
+     if(!container1[prop]) return false;
+     
+     if(container1[prop] < container2[prop]) return false;
+   }
+   return true;
+ }
+
+ 
