2175c29ce4403315bd7f905293fcb6e539bef2f4
Janice-M <janiceink001@gmail.com>
2022-08-30 15:23:53 +0300

    powers of two

--- datasstructures.js
+++ datasstructures.js
@@ -521,3 +521,13 @@ Complete the function that takes a non-negative integer n as input, and returns 
 
 
 */
+
+function powersOfTwo(n){
+  let result = [];
+  
+  for (let i = 0; i <= n; i++) {
+    result.push(2 ** i);
+  }
+  
+  return result;
+}
